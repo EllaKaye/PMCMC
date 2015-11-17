a9ce9db48e01dd39c6008f22c8795627c89f34a5
kasparmartens <kaspar.martens@gmail.com>
2015-11-17 16:58:30 +0000

    Acceptance plot update

--- PIMH_accept.R
+++ PIMH_accept.R
@@ -1,57 +1,47 @@
-### overnight plot
+library(ggplot2)
+
 # set up N and T
+source("data_generation.R")
 num_particles <- c(10, 50, 100, 200, 500, 1000, 2000)
 np <- length(num_particles)
 Time <- c(10, 25, 50, 100)
 
-# case 1, sv^2 = 10, sw^2 = 10
-sv <- sqrt(10)
-sw <- sqrt(10)
-accept.10.10 <- numeric(0)
-for (t in Time) {
-  set.seed(1)
-  data <- generate_model2(t, sv = sv, sw = sw)
-  Y <- data$Y
-  
-  for (n in num_particles) {
-    s <- PIMH(n, Y, dg, rf, rmu, iters=5000) # CHANGE TO WHATEVER FUNCTION YOU'RE USING - MAYBE UP ITERS
-    accept.10.10 <- c(accept.10.10, s$accept)
-    cat("n =", n, ", t =", t, "\n")
+create_plot = function(sv, sw, Time, num_particles){
+  res <- numeric(0)
+  for (t in Time) {
+    set.seed(1)
+    data <- generate_model2(t, sv = sv, sw = sw)
+    Y <- data$Y
+    
+    for (n in num_particles) {
+      s <- PIMH_fast(n, Y, sv, sw, iters=1000) 
+      res <- c(res, s$accept)
+      cat("n =", n, ", t =", t, "\n")
+    }
   }
+  df <- data.frame(N = num_particles, T = as.factor(rep(Time, each = np)), accept = res)
+  return(df)
 }
-accept.10.10
-
-df.10.10 <- data.frame(N = num_particles, T = as.factor(rep(Time, each = np)), accept = accept.10.10)
-p.10.10 <- ggplot(df.10.10, aes(N, accept)) + geom_line(aes(group=T, colour=T)) + geom_point(aes(group=T, colour=T))
-p.10.10
 
 # case 1, sv^2 = 10, sw^2 = 1
-sw <- 1
-accept.10.1 <- numeric(0)
-for (t in Time) {
-  set.seed(1)
-  data <- generate_model2(t, sv = sv, sw = sw)
-  Y <- data$Y
-  
-  for (n in num_particles) {
-    s <- PIMH(n, Y, dg, rf, rmu, iters=5000) # CHANGE TO WHATEVER FUNCTION YOU'RE USING - MAYBE UP ITERS
-    accept.10.1 <- c(accept.10.1, s$accept)
-    cat("n =", n, ", t =", t, "\n")
-  }
-}
-accept.10.1
-df.10.1 <- data.frame(N = num_particles, T = as.factor(rep(Time, each = np)), accept = accept.10.1)
-p.10.1 <- ggplot(df.10.1, aes(N, accept)) + geom_line(aes(group=T, colour=T)) + geom_point(aes(group=T, colour=T))
-p.10.1
+df1 = create_plot(sqrt(10), sqrt(10), Time, num_particles)
+
+# case 2, sv^2 = 10, sw^2 = 1
+df2 = create_plot(sqrt(10), sqrt(1), Time, num_particles)
 
 
 # join the two data frames and add variance column for facetting
-df <- rbind(cbind(df.10.10, variance = "sv^2 = 10, sw^2 = 10"),
-            cbind(df.10.1, variance = "sv^2 = 10, sw^2 = 1"))
+df <- rbind(cbind(df1, variance = "sv^2 = 10, sw^2 = 10"),
+            cbind(df2, variance = "sv^2 = 10, sw^2 = 1"))
 
 
 # MAKE ANY CHANGES TO PLOT APPEARANCE AS YOU SEE FIT.
-p.all <- ggplot(df, aes(N, accept)) + geom_line(aes(group=T, colour=T)) + geom_point(aes(group=T, colour=T)) + facet_grid(.~variance)
+p.all <- ggplot(df, aes(N, accept)) + 
+  geom_line(aes(group=T, colour=T)) + 
+  geom_point(aes(group=T, colour=T)) + 
+  facet_grid(.~variance) + 
+  ylim(0, 1) + 
+  theme_bw()
 pdf("pimh_accept.pdf", width = 8, height = 4)
 p.all
 dev.off()
