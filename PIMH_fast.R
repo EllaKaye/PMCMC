8436cccab79ca0787d5ad6f41f42ff32e7cac739
kasparmartens <kaspar.martens@gmail.com>
2015-11-17 15:18:35 +0000

    Add filtering means to PIMH fast

--- PIMH_fast.R
+++ PIMH_fast.R
@@ -1,13 +1,12 @@
 library(Rcpp)
 sourceCpp("SMC_fast.cpp")
-SMC_cpp(200, y = c(2, 2.5 ,3 ,3), 10, 10)
-
 
 PIMH_fast <- function(N, y, sv, sw, iters) {
   chain <- matrix(NA, iters+1, length(y))
   marginal.ll <- numeric(iters+1)
   accept <- 0
   u <- runif(iters)
+  filtering_means <- matrix(NA, iters+1, length(y))
 
   # step 1 (i = 0)
   s <- SMC_cpp(N, y, sv, sw)
@@ -15,6 +14,7 @@ PIMH_fast <- function(N, y, sv, sw, iters) {
   x.star <- s$X_updated[,index]
   marginal.ll[1] <- s$marginal.LL
   chain[1,] <- x.star
+  filtering_means[1,] <- s$filtering_means
 
   # step 2
   for (i in 1:iters) {
@@ -27,16 +27,17 @@ PIMH_fast <- function(N, y, sv, sw, iters) {
     if (u[i] <= exp(p.star-marginal.ll[i])) {
       chain[i+1,] <- x.star
       marginal.ll[i+1] <- p.star
+      filtering_means[i+1,] <- s$filtering_means
       accept <- accept + 1
     }
     else {
       chain[i+1,] <- chain[i,]
       marginal.ll[i+1] <- marginal.ll[i]
+      filtering_means[i+1,] <- filtering_means[i,]
     }
   }
   
   accept_prob <- accept/iters
-  return(list(chain = chain, marginal.ll = marginal.ll, accept = accept_prob))
+  return(list(chain = chain, marginal.ll = marginal.ll, accept = accept_prob, filtering_means=filtering_means))
 }
 
-PIMH_fast(N = 200, y = data$Y, sv = sqrt(10), sw = sqrt(10), iters = 1000)
